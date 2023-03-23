6bf0575f7e389a02abaf303beac9a0b8c5890fbb
Denis Ouma <dennisouma38@gmail.com>
2023-03-23 03:10:49 +0300

    records succesfully inserted to the database

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const { Client } = require("pg");
 
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+const port = 5000;
+
+const urls = [
+	{ url: "https://www.jumia.co.ke/sugar/", country: "Kenya" },
+	{ url: "https://www.jumia.ug/sugar/", country: "Uganda" },
+	{ url: "https://www.jumia.com.ng/sugars/", country: "Nigeria" },
+];
+
 const client = new Client({
 	user: "postgres",
 	host: "localhost",
@@ -9,11 +21,45 @@ const client = new Client({
 	port: 5432,
 });
 
-client.connect();
+async function scrapeData() {
+	try {
+		await client.connect();
 
-const app = express();
-const port = 5000;
+		for (const url of urls) {
+			const response = await axios.get(url.url);
+			const $ = cheerio.load(response.data);
 
-app.listen(port, () => {
-	console.log(`Server listening to ${port}`);
+			console.log(`Scraping data for ${url.country}`);
+
+			const promises = [];
+
+			$(".-paxs>article").each((index, sugar) => {
+				const sugarName = $(sugar).find(".info h3").text().trim();
+				const sugarPrice = $(sugar).find(".prc").text().trim();
+				const numericPrice = parseFloat(sugarPrice.match(/\d+(\.\d+)?/)[0]);
+				const currentDate = new Date().toISOString().slice(0, 10);
+
+				const insertQuery = {
+					text: "INSERT INTO sugar_prices (sugar_name, country, date, price) VALUES ($1, $2, $3, $4)",
+					values: [sugarName, url.country, currentDate, numericPrice],
+				};
+
+				const promise = client.query(insertQuery);
+				promises.push(promise);
+			});
+
+			await Promise.all(promises);
+			console.log(`Data inserted successfully for ${url.country}`);
+		}
+	} catch (error) {
+		console.error(error);
+	} finally {
+		await client.end();
+	}
+}
+
+scrapeData().then(() => {
+	app.listen(port, () => {
+		console.log(`Server listening to ${port}`);
+	});
 });
