1ca0e5377aaad7653a941e8041438654a4563555
Denis Ouma <dennisouma38@gmail.com>
2023-03-23 22:56:50 +0300

    adding end points for geting the sugar prices and getting the price of sugar based on the country

--- app.js
+++ app.js
@@ -11,6 +11,47 @@ const client = new Client({
 	port: 5432,
 });
 
+app.get("/prices", async (req, res) => {
+	try {
+		await client.connect();
+
+		const query = {
+			text: "SELECT sugar_name, country, date, price FROM sugar_prices WHERE date = (SELECT MAX(date) FROM sugar_prices)",
+		};
+		const result = await client.query(query);
+
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Internal server error");
+	} finally {
+		await client.end();
+	}
+});
+
+app.get("/prices/:country", async (req, res) => {
+	try {
+		await client.connect();
+
+		const query = {
+			text: "SELECT sugar_name, country, date, price FROM sugar_prices WHERE country = $1 AND date = (SELECT MAX(date) FROM sugar_prices WHERE country = $1)",
+			values: [req.params.country],
+		};
+		const result = await client.query(query);
+
+		if (result.rows.length === 0) {
+			res.status(404).send("Country not found");
+		} else {
+			res.json(result.rows);
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Internal server error");
+	} finally {
+		await client.end();
+	}
+});
+
 app.listen(port, () => {
 	console.log(`Server listening to ${port}`);
 });
