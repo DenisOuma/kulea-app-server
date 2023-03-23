b9bab6eaebc8b664a21bd69fd40b9554abeb4fbd
Denis Ouma <dennisouma38@gmail.com>
2023-03-23 23:34:28 +0300

    getting the latest sugar price

--- app.js
+++ app.js
@@ -3,54 +3,46 @@ const { Client } = require("pg");
 const app = express();
 const port = 5000;
 
-const client = new Client({
-	user: "postgres",
-	host: "localhost",
-	database: "sugarprice",
-	password: "denis123",
-	port: 5432,
+app.use((req, res, next) => {
+	const client = new Client({
+		user: "postgres",
+		host: "localhost",
+		database: "sugarprice",
+		password: "denis123",
+		port: 5432,
+	});
+	client.connect();
+	req.db = client;
+	next();
 });
 
-// Fetch all the sugar prices
 app.get("/prices", async (req, res) => {
 	try {
-		await client.connect();
-
-		const query = {
-			text: "SELECT sugar_name, country, date, price FROM sugar_prices WHERE date = (SELECT MAX(date) FROM sugar_prices)",
-		};
-		const result = await client.query(query);
-
+		const result = await req.db.query("SELECT * FROM sugar_prices");
 		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send("Internal server error");
-	} finally {
-		await client.end();
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Server error");
 	}
 });
 
-// Fetch sugar price based on country
 app.get("/prices/:country", async (req, res) => {
-	try {
-		await client.connect();
+	const { country } = req.params;
 
-		const query = {
-			text: "SELECT sugar_name, country, date, price FROM sugar_prices WHERE country = $1 AND date = (SELECT MAX(date) FROM sugar_prices WHERE country = $1)",
-			values: [req.params.country],
-		};
-		const result = await client.query(query);
+	try {
+		const result = await req.db.query(
+			"SELECT * FROM sugar_prices WHERE country = $1 ORDER BY date DESC LIMIT 1",
+			[country]
+		);
 
 		if (result.rows.length === 0) {
-			res.status(404).send("Country not found");
+			res.status(404).send(`No sugar prices found for ${country}`);
 		} else {
-			res.json(result.rows);
+			res.json(result.rows[0]);
 		}
-	} catch (err) {
-		console.error(err);
-		res.status(500).send("Internal server error");
-	} finally {
-		await client.end();
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Server error");
 	}
 });
 
